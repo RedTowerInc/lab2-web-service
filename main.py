64f91e72f4312fee39a8d71c84a3b533ce356477
Andy <andymurzykov70@gmail.com>
2025-01-29 19:12:43 +0700

    Добавлен веб-интерфейс для отображения комплектующих

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flasgger import Swagger
 from sitepart.sitepart import sitepart
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
-# Подключение части API
+# Подключаем API
 app.register_blueprint(sitepart, url_prefix="/api")
+
+
+@app.route('/')
+def index():
+    """Главная страница с таблицей комплектующих"""
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
